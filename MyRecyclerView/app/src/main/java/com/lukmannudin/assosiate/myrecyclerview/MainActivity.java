1d617cc4f3f4a48825a4296c67dae012b25e1f1d
Lukmannudin <laodelukmannudinpriatna@email.unikom.ac.id>
2018-05-11 00:07:49 +0700

    Update RecyclerView Ngulik

--- MyRecyclerView/app/src/main/java/com/lukmannudin/assosiate/myrecyclerview/MainActivity.java
+++ MyRecyclerView/app/src/main/java/com/lukmannudin/assosiate/myrecyclerview/MainActivity.java
@@ -2,8 +2,11 @@ package com.lukmannudin.assosiate.myrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -23,10 +26,39 @@ public class MainActivity extends AppCompatActivity {
         showRecyclerList();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_list:
+                showRecyclerList();
+                break;
+            case R.id.action_grid:
+                showRecyclerGrid();
+                break;
+            case R.id.action_cardView:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void showRecyclerList() {
         rvCategory.setLayoutManager(new LinearLayoutManager(this));
         ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
         listPresidentAdapter.setListPresident(list);
         rvCategory.setAdapter(listPresidentAdapter);
+    }
+
+    private void showRecyclerGrid(){
+        rvCategory.setLayoutManager(new GridLayoutManager(this,2));
+        GridPresidentAdapter gridPresidentAdapter = new GridPresidentAdapter(this);
+        gridPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(gridPresidentAdapter);
     }
 }
