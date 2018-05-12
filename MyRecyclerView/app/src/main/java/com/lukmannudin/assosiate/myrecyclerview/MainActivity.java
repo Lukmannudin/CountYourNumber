864d79136dee406d58378c108d5666026d3624b6
Lukmannudin <laodelukmannudinpriatna@email.unikom.ac.id>
2018-05-12 18:54:34 +0700

    RecyclerView Finished

--- MyRecyclerView/app/src/main/java/com/lukmannudin/assosiate/myrecyclerview/MainActivity.java
+++ MyRecyclerView/app/src/main/java/com/lukmannudin/assosiate/myrecyclerview/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setActionBarTitle("Mode List");
         rvCategory = findViewById(R.id.rv_category);
         rvCategory.setHasFixedSize(true);
 
@@ -34,18 +36,28 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        String title = null;
         switch (item.getItemId()){
             case R.id.action_list:
                 showRecyclerList();
+                title = "Mode List";
                 break;
             case R.id.action_grid:
                 showRecyclerGrid();
+                title = "Mode Grid";
                 break;
             case R.id.action_cardView:
+                showRecyclerCardView();
+                title = "Mode CardView";
                 break;
 
         }
+        setActionBarTitle(title);
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setActionBarTitle(String title) {
+        getSupportActionBar().setTitle(title);
     }
 
     private void showRecyclerList() {
@@ -53,6 +65,13 @@ public class MainActivity extends AppCompatActivity {
         ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
         listPresidentAdapter.setListPresident(list);
         rvCategory.setAdapter(listPresidentAdapter);
+
+        ItemClickSupport.addTo(rvCategory).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                showSelectedPresident(list.get(position));
+            }
+        });
     }
 
     private void showRecyclerGrid(){
@@ -60,5 +79,24 @@ public class MainActivity extends AppCompatActivity {
         GridPresidentAdapter gridPresidentAdapter = new GridPresidentAdapter(this);
         gridPresidentAdapter.setListPresident(list);
         rvCategory.setAdapter(gridPresidentAdapter);
+
+        ItemClickSupport.addTo(rvCategory).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                showSelectedPresident(list.get(position));
+            }
+        });
     }
+
+    private void showRecyclerCardView(){
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        CardViewPresidentAdapter cardViewPresidentAdapter = new CardViewPresidentAdapter(this);
+        cardViewPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(cardViewPresidentAdapter);
+    }
+
+    private void showSelectedPresident(President president){
+        Toast.makeText(this,"Kamu memilih "+president.getName(), Toast.LENGTH_SHORT).show();
+    }
+
 }
